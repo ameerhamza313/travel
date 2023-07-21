8116eb18a52ce45dce3637cb78479e87bc7c9504
Ameerhamza Sanadi <ameerhamzasanadi@gmail.com>
2023-07-21 23:37:16 +0530

    login page added login page added

--- script.js
+++ script.js
@@ -32,3 +32,20 @@ formBtn.addEventListener('click', () =>{
 formClose.addEventListener('click', () =>{
     loginForm.classList.remove('active');
 });
+
+function logout() {
+    // Clear the logged-in status in localStorage
+    localStorage.removeItem('isLoggedIn');
+
+    // Redirect back to the login page
+    window.location.href = 'login.html';
+  }
+
+  // Check if the user is logged in when the index.html page loads
+  document.addEventListener('DOMContentLoaded', function () {
+    const isLoggedIn = localStorage.getItem('isLoggedIn');
+    if (!isLoggedIn || isLoggedIn !== 'true') {
+      // Redirect back to the login page if not logged in
+      window.location.href = 'login.html';
+    }
+  });
