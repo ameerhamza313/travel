1223bbd94fa9de2feb725245a4ebd41a2b8f5b20
Ameerhamza Sanadi <ameerhamzasanadi@gmail.com>
2023-08-18 09:59:41 +0530

    Updated

--- script.js
+++ script.js
@@ -49,3 +49,39 @@ function logout() {
       window.location.href = 'index.html';
     }
   });
+
+
+
+  document.getElementById("bookingForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(this);
+    const fullname = formData.get("fullname");
+    const destination = formData.get("destination");
+    const numPeople = formData.get("numPeople");
+    const arrivalDate = formData.get("arrivalDate");
+    const departureDate = formData.get("departureDate");
+    const to_email = formData.get("email"); // Using the entered email as the recipient
+
+    // Initialize EmailJS with your user ID
+    emailjs.init("sp7Pj81e0ewvULQV6");
+
+    emailjs.send("service_zd8dy7o", "template_rvt5s0i", {
+        fullname: fullname,
+        destination: destination,
+        numPeople: numPeople,
+        arrivalDate: arrivalDate,
+        departureDate: departureDate,
+        to_email: to_email
+    })
+    .then(function(response) {
+        console.log("Email sent successfully:", response);
+        alert('Booking Successful');
+        // Add your success message or further actions here
+    })
+    .catch(function(error) {
+        console.error("Error sending email:", error);
+        alert('Error');
+        // Add your error handling here
+    });
+});
